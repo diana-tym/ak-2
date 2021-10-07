adfecd3e2ab87a4a0f3ddb9821d6612ad3757ce0
Diana Tym <ausherika@gmail.com>
2021-10-07 11:10:10 +0300

    add comments

--- calculator.cpp
+++ calculator.cpp
@@ -1,17 +1,18 @@
 #include "calculator.h"
 
+// функція для додавання
 int Calculator::Add (double a, double b)
 {
-
     return a + b;
-
 }
 
+// функція для віднімання
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// функція для множення
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
